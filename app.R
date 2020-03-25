8e27b6922c76dccaae765a353f2d22b850c9ff68
JUNGHO YEOM <junghoyeom@JUNGHOs-Air.fios-router.home>
2020-03-25 12:27:46 -0400

    add days since 100 plot

--- app.R
+++ app.R
@@ -1,26 +1,5 @@
 # Load libraries ---------------------------------------------------
 
-# library(shiny)
-# library(shinyWidgets)
-# library(shinydashboard)
-# library(shinythemes)
-# library(tidyverse)
-# library(lubridate)
-# library(plotly)
-
-# if (!require("pacman")) install.packages("pacman")
-# pacman::p_load(shiny, 
-#                shinyWidgets, 
-#                shinydashboard,
-#                shinythemes,
-#                tidyverse,
-#                lubridate,
-#                plotly,
-#                leaflet,
-#                leaflet.extras,
-#                tigris,
-#                RColorBrewer)
-
 library(shiny)
 library(shinyWidgets)
 library(shinydashboard)
@@ -45,44 +24,58 @@ cv_us <- read_csv(url(url))
 today <- as.Date(Sys.Date())
 string_today <- paste(str_sub(today,6,7), str_sub(today,9,10), year(today), sep = '-')
 
-
 url <- paste0('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/', string_today, '.csv')
-cv_us_today <- read_csv(url(url))
+cv_us_today <- tryCatch(read_csv(url(url)), warn = F,
+                        error = function(e) NULL)
 
 cv_us <- cv_us %>%
   mutate(countyFIPS = str_pad(countyFIPS,5,'left', '0')) %>%
   distinct(countyFIPS, .keep_all = T) %>% # Distinct county codes
   mutate(countyFIPS = ifelse(countyFIPS == '46031', '45031', countyFIPS)) # Darlington county FIPS code fixed
 
-cv_us_today <- cv_us_today %>%
-  rename(countyFIPS = FIPS,
-         today = Confirmed,
-         last_update = Last_Update) %>%
-  select(countyFIPS, today, last_update) %>%
-  distinct(countyFIPS, .keep_all = T)
+if(!is.null(cv_us_today)){
+  cv_us_today <- cv_us_today %>%
+    rename(countyFIPS = FIPS,
+           today = Confirmed,
+           last_update = Last_Update) %>%
+    select(countyFIPS, today, last_update) %>%
+    distinct(countyFIPS, .keep_all = T)
+}
   
 ## combine historical and today
 
-cv_us <- cv_us %>%
-  left_join(cv_us_today, by = 'countyFIPS')
-
-
-
+if(!is.null(cv_us_today)){
+  cv_us_today <- cv_us_today %>%
+  left_join(cv_us, by = 'countyFIPS')
+} else{
+  cv_us_today <- cv_us
+}
+  
 cty_data <- cty@data
 cty_data <- cty_data %>%
   mutate(countyFIPS = paste0(STATEFP, COUNTYFP)) %>%
-  left_join(cv_us, by = c('countyFIPS' = 'countyFIPS')) %>%
+  left_join(cv_us_today, by = c('countyFIPS' = 'countyFIPS')) %>%
   mutate_if(is.numeric, function(x) replace_na(x, 0)) # Replace all NA to zero
 
+cty_data <- cty_data %>%
+  select(1:10,12:ncol(cty_data),11)
+
+col_index <- grep('^[0-9]|^today',names(cty_data))
+
 cty_data_gather <- cty_data %>%
-  tidyr::gather(date,value,14:ncol(cty_data)) %>%
+  tidyr::gather(date,value,col_index) 
+
+cty_data_gather <- cty_data_gather %>%
+  mutate(date = ifelse(date == 'today', paste(month(today), day(today), year(today), sep = '/'), date))
+
+cty_data_gather <- cty_data_gather %>%
   mutate(date = as.Date(date, format = '%m/%d/%Y'))
 
 cty@data <- cty_data
 
 # Palette for color coding
 bins <- c(-Inf, 0, 10, 100, 500, Inf) # create color bins
-pal <- colorBin("BuGn", domain = cty@data$`3/20/2020`, bins = bins)
+pal <- colorBin("BuGn", domain = cty_data[, ncol(cty_data)-1], bins = bins)
 
 # State name append
 
@@ -110,13 +103,44 @@ covid_by_ctry <- covid %>%
   summarise_if(is.numeric, sum, na.rm = T) %>%
   rename(country = `Country/Region`)
 
+covid_by_ctry <- covid_by_ctry[covid_by_ctry[, ncol(covid_by_ctry)] > 200,]
+
 covid_tidy <- covid_by_ctry %>%
   tidyr::gather(date,value,4:ncol(covid_by_ctry))
+
+# Data frame for COVID19 days since 100
+
+covid_tidy_days_since_100 <- covid_tidy %>%
+  filter(value > 100) %>%
+  group_by(country) %>%
+  mutate(days = 1) %>%
+  mutate(days_since_100 = cumsum(days))
+
+# Data frame for COVID19 today
 
 cv_today <- covid_tidy %>%
   mutate(date = as.Date(date, format = '%m/%d/%Y')) %>%
   filter(date == max(date)) %>%
   arrange(-value)
+
+# annotation_df <- covid_tidy_days_since_100 %>%
+#   group_by(country) %>%
+#   filter(days_since_100 == max(days_since_100))
+# 
+# covid_tidy_days_since_100 %>%
+#   group_by(country) %>%
+#   plot_ly(x = ~days_since_100) %>%
+#   add_lines(y = ~log(value), color = ~factor(country)) %>%
+#   layout(xaxis = x, yaxis = y) %>%
+#   add_annotations(x = annotation_df$days_since_100,
+#                   y = log(annotation_df$value),
+#                   color = ~factor(annotation_df$country),
+#                   text = annotation_df$country,
+#                   showarrow = F,
+#                   ax = 30,
+#                   font = list(family = 'sans serif',
+#                               size = 14))
+  
 
 # Country Case plot function -----------------------------------------
 
@@ -145,7 +169,7 @@ country_cases_plot = function(cv_cases, type = 'cumulative') {
       add_lines(y = ~value, color = ~factor(country)) %>%
       layout(xaxis = x, yaxis = y)
   } 
-   
+  
   if(type == 'new'){
     plot = cv_cases %>%
       mutate(date = as.Date(date, format = "%m/%d/%Y")) %>%
@@ -167,6 +191,18 @@ country_cases_plot = function(cv_cases, type = 'cumulative') {
       plot_ly(x = ~date) %>%
       add_lines(y = ~log_cum, color = ~factor(country)) %>%
       layout(xaxis = x, yaxis = y)
+  }
+  
+  if(type == 'days_since_100'){
+    annotation_df <- cv_cases %>%
+      group_by(country) %>%
+      filter(days_since_100 == max(days_since_100))
+    
+    plot = cv_cases %>%
+      group_by(country) %>%
+      plot_ly(x = ~days_since_100) %>%
+      add_lines(y = ~log(value), color = ~factor(country)) %>%
+      layout(xaxis = x, yaxis = y) 
   }
   plot
 }
@@ -201,7 +237,8 @@ ui <- navbarPage(theme = shinytheme("darkly"), collapsible = TRUE,
                               absolutePanel(id = "controls", class = "panel panel-default",
                                             top = 80, left = 20, width = 350, fixed=TRUE,
                                             draggable = TRUE, height = 'auto',
-                                            h3(textOutput("reactive_case_count"), align = "right", style="color:#006d2c"),
+                                            h3(textOutput("reactive_case_count"), align = "center", style="color:#006d2c"),
+                                            h5(textOutput("reactive_last_update"), align = "center", style="color:#006d2c"),
                                             h5('Select date for mapping', style='color:#000000'),
                                             sliderInput("plot_date",
                                                         label = 'Select date for mapping',
@@ -235,15 +272,16 @@ ui <- navbarPage(theme = shinytheme("darkly"), collapsible = TRUE,
                             
                             mainPanel(
                               tabsetPanel(
-                                tabPanel("New", plotlyOutput("country_plot", height = 'auto')),
-                                tabPanel("Cumulative", plotlyOutput("country_plot_cumulative", height = 'auto')),
-                                tabPanel("Cumulative (log10)", plotlyOutput("country_plot_cumulative_log", height = 'auto'))
+                                tabPanel("New", plotlyOutput("country_plot", height = '800px')),
+                                tabPanel("Cumulative", plotlyOutput("country_plot_cumulative", height = '800px')),
+                                tabPanel("Cumulative (log10)", plotlyOutput("country_plot_cumulative_log", height = '800px')),
+                                tabPanel("Days sine 100", plotlyOutput("country_plot_days_since_100", height = '800px'))
                               )
                             )
                           )
                  ),
                  
-
+                 
                  tabPanel("About this site",
                           tags$div(
                             tags$h4("Last update"),
@@ -264,8 +302,14 @@ server = function(input, output) {
     cty_data_gather = cty_data_gather %>% filter(date == input$plot_date)
     cty@data <- cty_data_gather
     cty@data$INFECTED <- cty@data$value
-    cty = subset(cty, State %in% input$state_select)
-    cty
+    
+    if(is.null(input$state_select)){
+      cty@data$INFECTED = 0
+      return(cty)
+    } else{
+      cty = subset(cty, State %in% input$state_select)
+      return(cty)
+    }
   })
   
   # Count in Selected State
@@ -273,6 +317,10 @@ server = function(input, output) {
     paste0(prettyNum(sum(reactive_covid()@data$INFECTED), big.mark=","), " cases")
   })
   
+  # Last Update
+  output$reactive_last_update <- renderText({
+    paste0("Last Updated: ", unique(reactive_covid()@data$last_update)[1])
+  })
   
   # Base leaflet map
   output$mymap <- renderLeaflet({ 
@@ -282,9 +330,9 @@ server = function(input, output) {
   # Reactive Label
   labels <- reactive({
     sprintf(
-    "<strong>%s</strong><br/>%g Confirmed Cases",
-    reactive_covid()@data$NAME, reactive_covid()@data$INFECTED
-  ) %>% lapply(htmltools::HTML)
+      "<strong>%s</strong><br/>%g Confirmed Cases",
+      reactive_covid()@data$NAME, reactive_covid()@data$INFECTED
+    ) %>% lapply(htmltools::HTML)
   })
   
   # Observe Event
@@ -301,7 +349,7 @@ server = function(input, output) {
                     style = list("font-weight" = "normal"),
                     textsize = "15px",
                     direction = "auto"))
-
+    
   })
   
   # covid by country
@@ -310,8 +358,24 @@ server = function(input, output) {
     covid_tidy %>% filter(country %in% input$country_select)
   })
   
+  # covid US days since 100
+  # reactive 
+  reactive_annot = reactive({
+    covid_tidy_days_since_100 %>%
+      filter(country %in% input$country_select) %>%
+      group_by(country) %>%
+      filter(days_since_100 == max(days_since_100))  
+  })
+  
+  # reactive 
+  reactive_days_100 = reactive({
+    covid_tidy_days_since_100 %>%
+      filter(country %in% input$country_select)
+  })
+  
+  
   # Plotly New/Cumlative/Cumulative Log Cases by Country
-
+  
   output$country_plot_cumulative_log <- renderPlotly({
     country_cases_plot(covid_by_ctry(), 'log_cumulative')
   })
@@ -321,13 +385,27 @@ server = function(input, output) {
   output$country_plot_cumulative <- renderPlotly({
     country_cases_plot(covid_by_ctry(), 'cumulative')
   })
+  output$country_plot_days_since_100 <- renderPlotly({
+    country_cases_plot(reactive_days_100(), 'days_since_100') %>%
+      add_annotations(x = reactive_annot()$days_since_100,
+                      y = log(reactive_annot()$value),
+                      color = ~factor(reactive_annot()$country),
+                      text = reactive_annot()$country,
+                      showarrow = F,
+                      ax = 30,
+                      font = list(family = 'sans serif',
+                                  size = 14))
+  })
   
 }
 
 shinyApp(ui, server)
 
-# library(rsconnect)
-# deployApp('/home/jungho/deep_learning/covid19/')
+library(rsconnect)
+
+
+
+deployApp('/Users/junghoyeom/Desktop/projects/covid19/')
 
 
 
